347261f2ff013990d0a6e263d35c3304b8d594a6
Ashleigh <ashleighc207@gmail.com>
2019-06-23 13:48:03 -0400

    complete logic for random icons

--- Icons.js
+++ Icons.js
@@ -1,14 +1,20 @@
+function reload() {
+	window.location.reload();
+}
 class Icons extends React.Component {
 	render() {
+		const props = this.props;
 		return (
-			<section className="outer-box">
-				<section className="inner-box">
-					<i className="fas fa-hamburger"></i>
-					<i className="fas fa-hamburger"></i>
-					<i className="fas fa-hamburger"></i>
+			<section>
+				<section className="outer-box">
+					<section className="inner-box">
+						<i ref="icon1" className={"fas " + props.i1}></i>
+						<i ref="icon2" className={"fas " + props.i2}></i>
+						<i ref="icon3" className={"fas " + props.i3}></i>
+					</section>
 				</section>
+				<button className="spin" type="button" onClick={reload}>Spin!</button>
 			</section>
-			<button className="spin" type="button">Spin!</button>
 		)
 	}
 }
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 let root = document.getElementById('root')
+let icons = ["fa-hamburger", "fa-pizza-slice", "fa-egg", "fa-cheese", "fa-bread-slice", "fa-bacon", "fa-hotdog", "fa-drumstick-bite", "fa-ice-cream"];
+
+let icon1 = icons[Math.floor(Math.random()*icons.length)];
+let icon2 = icons[Math.floor(Math.random()*icons.length)];
+let icon3 = icons[Math.floor(Math.random()*icons.length)];
+
 
 class App extends React.Component {
 	render() {
 		return (
 			<main className="main-content">
-				<h1 className="heading-one"> Are you lucky? </h1>
-				<section className="outer-box">
-					<section className="inner-box">
-						<i className="fas fa-hamburger"></i>
-						<i className="fas fa-hamburger"></i>
-						<i className="fas fa-hamburger"></i>
-					</section>
-				</section>
-				<button className="spin" type="button">Spin!</button>
+				<h1 className="heading-one"> Are you feeling lucky?</h1>
+				<Icons i1={icon1} i2={icon2} i3={icon3}/>
+				{(icon1 == icon2 && icon2 == icon3) ? <h1> Winner!! </h1> : <h1> Try again </h1> }
 			</main>
 		)
 	}
