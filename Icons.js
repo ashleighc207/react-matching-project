7b36996a64ed821dd48b39d3742c4ba76508c08e
Ashleigh <ashleighc207@gmail.com>
2019-06-23 13:27:56 -0400

    split app into components

---
+++ Icons.js
@@ -0,0 +1,15 @@
+class Icons extends React.Component {
+	render() {
+		return (
+			<section className="outer-box">
+				<section className="inner-box">
+					<i className="fas fa-hamburger"></i>
+					<i className="fas fa-hamburger"></i>
+					<i className="fas fa-hamburger"></i>
+				</section>
+			</section>
+			<button className="spin" type="button">Spin!</button>
+		)
+	}
+}
+
